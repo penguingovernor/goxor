5dcc89fba9efd2a39c5b6f917f0022496c9e2b9d
Jorge Henriquez <joahenri@ucsc.edu>
2017-09-16 00:35:45 -0700

    Initial bring-up

---
+++ README.md
@@ -0,0 +1,35 @@
+# goxor 
+GoXor is an encryption method written in the [Go programming language](https://golang.org/), and is based on a [xor cipher](https://en.wikipedia.org/wiki/XOR_cipher) in conjunction with a [one-time pad](https://en.wikipedia.org/wiki/One-time_pad). 
+
+### Download and Install 
+1. Install and configure Go for you operating system. [See Here](https://golang.org/doc/install)
+2. Once Go is installed and configured, run this command to install the `goxor` tool 
+
+```shell
+go get github.com/penguingovernor/goxor
+``` 
+
+### Documentation 
+After installing, you can use `go doc` to get documenation:
+```shell
+go doc github.com/penguingovernor/goxor
+```
+
+### Running goxorcrypt 
+`goxor` can be run in two modes of operation: encrypt and decrypt mode.
+
+Example:
+```shell
+# Encrypting a file 
+goxor --input="exampleFile" # 'goxor -i exampleFile' works too!
+
+# Decrypting a file 
+goxor --decrypt --input="exampleFile.gxef" # Again, 'goxor -d -i exampleFile.gxef' works as well!
+```
+
+The `goxor` command can encrypt and decrypt files. It supports the following flags:
+
+* `--decrypt` or `-d`: Toggle decryption mode. Omitting this flag causes `goxor` to be encryption mode.
+* `--input=` or `-i`: The file to encrypted or decrypted. In encryption mode, omitting this flag causs `goxor` to read from stdin
+* `--output=` or `-o`: The desired file name to output the encrypted or decrypted data to. Omitting this flag in encryption mode causes `goxor` to output to fileName.gxef. Omitting this flag in decryption mode causes `goxor` to output to stdout.
+* `--clean` or `-c`: Remove encrypted data and key file upon completion of decryption. 
---
+++ main.go
@@ -0,0 +1,173 @@
+// GoXor is a command line tool that encrypts and decrypts data.
+//
+// For usagae run `goxor --help`
+//
+// Encryption Specifics:
+// Unless specified otherwise, encrypting a file yields a file with a go xor encrypted file extension (.gxef)
+// and a key (.gxef.key) with the original file name preceding the extension.
+// It is crucial the the key file is not renamed as `goxor` will look for this file in decryption.
+//
+// Without the key file the encrypted data is utterly useless, unless of coure you're a cryptanalyst.
+package main
+
+import (
+	"crypto/rand"
+	"fmt"
+	"io/ioutil"
+	"log"
+	"os"
+
+	"github.com/ogier/pflag"
+)
+
+// Flag Related Code
+
+var (
+	inputFile   string
+	outputFile  string
+	decryptFlag bool
+	cleanFlag   bool
+)
+
+func init() {
+	pflag.BoolVarP(&decryptFlag, "decrypt", "d", false, "Toggle decrypt mode")
+	pflag.BoolVarP(&cleanFlag, "clean", "c", false, "Remove decrypted data and key among completion")
+	pflag.StringVarP(&inputFile, "input", "i", "", "Input file")
+	pflag.StringVarP(&outputFile, "output", "o", "", "Output file")
+}
+
+func helpMsg() string {
+	return "Try 'name --help' for more information"
+}
+
+func verifyFlags() {
+	pflag.Parse()
+	if !decryptFlag && inputFile == "" && outputFile == "" {
+		log.Fatalf("\nNo output file while trying to read from stdin\n%s\n", helpMsg())
+	}
+	if decryptFlag && inputFile == "" {
+		log.Fatalf("\nNo input file while trying to to decrypt\n%s\n", helpMsg())
+	}
+}
+
+// End Flag Related Code
+
+// File Related Code
+func getFileBytes(fileName string) []byte {
+	var (
+		fileBytes []byte
+		err       error
+	)
+
+	if fileName == "" {
+		fileBytes, err = ioutil.ReadAll(os.Stdin)
+	} else {
+		fileBytes, err = ioutil.ReadFile(fileName)
+	}
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	return fileBytes
+
+}
+
+func writeEncryptedData(data []byte) {
+	var fileName string
+	if outputFile == "" {
+		fileName = inputFile + ".gxef"
+	} else {
+		fileName = outputFile
+	}
+	ioutil.WriteFile(fileName, data, 0666)
+}
+
+func writeKey(key []byte) {
+	var fileName string
+	if outputFile == "" {
+		fileName = inputFile + ".gxef.key"
+	} else {
+		fileName = outputFile + ".key"
+	}
+	ioutil.WriteFile(fileName, key, 0666)
+}
+
+func writeDecryptedData(data []byte) {
+	if outputFile == "" {
+		fmt.Print(string(data))
+		return
+	}
+	ioutil.WriteFile(outputFile, data, 0666)
+}
+
+func removeEvidence() {
+	fmt.Println("Cleaning up files...")
+	err := os.Remove(inputFile)
+	if err != nil {
+		log.Fatal(err)
+	}
+	err = os.Remove(inputFile + ".key")
+	if err != nil {
+		log.Fatal(err)
+	}
+}
+
+// End File Related Code
+
+// Encryption Code
+
+func encrytFile() {
+	fileBytes := getFileBytes(inputFile)
+	fileKey := generateFileKey(len(fileBytes))
+	encrytedBytes := encrypt(fileBytes, fileKey)
+	writeEncryptedData(encrytedBytes)
+	writeKey(fileKey)
+}
+
+func generateFileKey(n int) []byte {
+	fileKey := make([]byte, n)
+	rand.Read(fileKey)
+	return fileKey
+}
+
+func encrypt(bytes, key []byte) []byte {
+	encryptedBytes := make([]byte, len(key))
+
+	for i := range encryptedBytes {
+		encryptedBytes[i] = bytes[i] ^ key[i]
+	}
+
+	return encryptedBytes
+
+}
+
+// End Encryption Code
+
+// Decryption Code
+
+func decryptFile() {
+	encryptedBytes := getFileBytes(inputFile)
+	fileKey := getFileBytes(inputFile + ".key")
+	decryptedData := decrypt(encryptedBytes, fileKey)
+	writeDecryptedData(decryptedData)
+	if cleanFlag {
+		removeEvidence()
+	}
+}
+
+func decrypt(encryptedData, key []byte) []byte {
+	return encrypt(encryptedData, key)
+}
+
+// End Decryption Code
+
+func main() {
+	verifyFlags()
+
+	if !decryptFlag {
+		encrytFile()
+	} else {
+		decryptFile()
+	}
+}
